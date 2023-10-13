d2dbb7f89a869226f3e509051767a674a5db7c30
diabGarren <garrendiab@gmail.com>
2023-10-13 21:02:09 +0200

    Fixed api

--- app/api/students/route.tsx
+++ app/api/students/route.tsx
@@ -11,7 +11,7 @@ export async function GET(req: any) {
         const limit_str = req.nextUrl.searchParams.get("limit");
 
         const page = page_str ? parseInt(page_str, 10) : 1;
-        const limit = limit_str ? parseInt(limit_str, 10) : 10;
+        const limit = limit_str ? parseInt(limit_str, 10) : 50;
 
         const { students, results, err } = await getStudents({ page, limit });
 
--- lib/students-db.tsx
+++ lib/students-db.tsx
@@ -2,17 +2,17 @@ import { Students } from "@/models/students";
 import connectDb from "./connectDb";
 import { stringToObjectId } from "./utils";
 
-interface UsersFilter {
+interface StudentsFilter {
     page?: number;
     limit?: number;
 }
 
-export async function getStudents(filter: UsersFilter = {}) {
+export async function getStudents(filter: StudentsFilter = {}) {
     try {
         await connectDb();
 
         const page = filter.page ?? 1;
-        const limit = filter.limit ?? 10;
+        const limit = filter.limit ?? 50;
         const skip = (page - 1) * limit;
 
         const students = await Students.find()
@@ -29,107 +29,6 @@ export async function getStudents(filter: UsersFilter = {}) {
             limit,
             results,
         };
-    } catch (err) {
-        return { err };
-    }
-}
-
-export async function createStudent(
-    name: string,
-    quote: string,
-    image: string,
-    colours: string[]
-) {
-    try {
-        await connectDb();
-
-        const students = await Students.create({
-            name: name,
-            quote: quote,
-            image: image,
-            colours: colours,
-        });
-
-        return {
-            students,
-        };
-    } catch (err) {
-        return { err };
-    }
-}
-
-export async function getStudent(id: string) {
-    try {
-        await connectDb();
-        const parsedId = stringToObjectId(id);
-
-        if (!parsedId) {
-            return { error: "Student not found" };
-        }
-
-        const students = await Students.findById(parsedId).lean().exec();
-        if (students) {
-            return { students };
-        } else {
-            return { error: "Student not found" };
-        }
-    } catch (err) {
-        return { err };
-    }
-}
-
-export async function updateStudent(
-    id: string,
-    {
-        name,
-        quote,
-        image,
-        colors,
-    }: { name: string; quote: string; image: string; colors: string[] }
-) {
-    try {
-        await connectDb();
-
-        const parsedId = stringToObjectId(id);
-
-        if (!parsedId) {
-            return { error: "Student not found" };
-        }
-
-        const students = await Students.findByIdAndUpdate(
-            parsedId,
-            { name, quote, image, colors },
-            { new: true }
-        )
-            .lean()
-            .exec();
-        if (students) {
-            return { students };
-        } else {
-            return { error: "Student not found" };
-        }
-    } catch (err) {
-        return { err };
-    }
-}
-
-export async function deleteStudent(id: string) {
-    try {
-        await connectDb();
-
-        const parsedId = stringToObjectId(id);
-
-        if (!parsedId) {
-            return { error: "Student not found" };
-        }
-
-        const students = await Students.findByIdAndDelete(parsedId).exec();
-
-        if (students) {
-            return {};
-        } else {
-            return { error: "Student not found" };
-        }
     } catch (err) {
         return { err };
     }
